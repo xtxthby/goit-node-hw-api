df647ce87bb7f5765bdde0747c26531702706a03
Yurii <116266510+xtxthby@users.noreply.github.com>
2023-07-30 16:47:21 +0300

    add files

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
+// це наш вебсервер
 const express = require('express')
+// 'morgan--це спеціальний мідлеваре який виводить вконсоль 
+// інформацію про запит це потрібно щоб дебажити код
 const logger = require('morgan')
 const cors = require('cors')
-
+// імпортуємоз файла contacts де вказані шляхи
 const contactsRouter = require('./routes/api/contacts')
-
+// створюємо вебсервер
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
+// прописуємо міделваре
 app.use(cors())
 app.use(express.json())
-
+// всі виклики які приходять на початковий 
+// адрес перенаправляти на contactsRouter
 app.use('/api/contacts', contactsRouter)
-
+// відповідь на адресу якої немає
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
-
+// це едина функція з 4-мя параметрами для виведення помилок
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message })
 })
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+// імпортуємо арр
 const app = require('./app')
-
+// запускаємо на локалхості 3000
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
